396fcaf951ea6259bd9c87c52b1839a3c0989493
DEVANAND GIRI <102689281+gdev8584@users.noreply.github.com>
2025-06-28 17:25:37 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ connectDB();
 app.use(express.json());
 app.use(cors());
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/test1", testRouter);
+app.use("/api/v1/test", testRouter);
 // add validation middelware
 app.use(errorMiddelware);
 // sample api
